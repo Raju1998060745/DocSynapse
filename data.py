b3f2599d6125b73e18e3d5309468bf13faef46ce
Raju1998060745 <99238367+Raju1998060745@users.noreply.github.com>
2025-01-26 14:02:23 -0500

    initial commit

---
+++ data.py
@@ -0,0 +1,7 @@
+from langchain_community.document_loaders import S3FileLoader
+from langchain_community.document_loaders import DropboxLoader
+# Generate access token: https://www.dropbox.com/developers/apps/create.
+dropbox_access_token = ""
+# Dropbox root folder
+dropbox_folder_path = ""
+#
