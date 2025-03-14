97b41d41ad7f1809f15753ac3e554a837ed7ec23
ManishReddyKovvuri <47208302+ManishReddyKovvuri@users.noreply.github.com>
2025-03-14 13:11:32 -0400

    Refactor document loading logic and update split document parameters; add test endpoint for document loading

--- service_2/main.py
+++ service_2/main.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI
 
-from .utils.document_utils import load_and_split_documents
+from service_2.utils.document_utils import load_and_split_documents
 app = FastAPI()
 
+load_and_split_documents('')
 
 
 @app.get("/home")
 async def index():
    return {"message": "Hello Worldhi "}
 
+
+@app.get("/testload")
+async def index():
+   return load_and_split_documents('')
 
 @app.get("/load")
 async def index(uuid : str , file_loc : str = None):
